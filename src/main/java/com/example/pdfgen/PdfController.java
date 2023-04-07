96582307c3de6dd687001ce748579255c40edb4e
Pavan <pavank1567>
2023-04-07 20:48:25 +0530

    first commit

---
+++ src/main/java/com/example/pdfgen/PdfController.java
@@ -0,0 +1,33 @@
+package com.example.pdfgen;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Controller
+public class PdfController {
+
+    @GetMapping("/pdf")
+    public ResponseEntity<Resource> downloadPdf() throws IOException {
+        String filePath="/home/pavan/Documents/sample.pdf";
+        File file = new File(filePath);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(resource);
+    }
+}
---
+++ src/main/java/com/example/pdfgen/PdfgenApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdfgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfgenApplication.class, args);
+	}
+
+}
